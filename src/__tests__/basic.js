60567b1b9193f0a148da063fcc08d5d4afc82b37
klim <klim.volchenkov.04@bk.ru>
2024-01-16 08:49:12 +0300

    init

---
+++ src/__tests__/basic.js
@@ -0,0 +1,43 @@
+import{ calculateTotal } from '../calculate.js';
+test("basic test", () => {
+    const result = 4;
+    expect(result).toBe(4);
+});
+
+test("calculateTotal sum without discount", () =>{
+    const list = [
+        {
+            id: 456,
+            name: "Война миров",
+            count: 3,
+            price: 400
+        },
+        {
+            id: 77,
+            name: "Js book",
+            count: 1,
+            price: 1300
+        }
+    ];
+    const result = calculateTotal(list);
+    expect(result).toBe(2500);
+})
+
+test("calculateTotal sum with discount", () =>{
+    const list = [
+        {
+            id: 456,
+            name: "Война миров",
+            count: 3,
+            price: 400
+        },
+        {
+            id: 77,
+            name: "Js book",
+            count: 1,
+            price: 1300
+        }
+    ];
+    const result = calculateTotal(list, true);
+    expect(result).toBe(1250);
+})
